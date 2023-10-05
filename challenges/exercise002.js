21ee7d11ad2d11c6589dd436f21fd7ce8cdb6e46
Adrian Yates <adeyatz@gmail.com>
2023-10-05 18:44:30 +0100

    feat: impl. isFromManchester  function

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -6,7 +6,8 @@ export function getFillings(sandwich) {
 
 export function isFromManchester(person) {
 	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	
+	return person.city === 'Manchester';
 }
 
 export function getBusNumbers(people) {
