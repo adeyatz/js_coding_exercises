21888f3dbca32c6fbbbd0e011e63b7d2889c2b17
Adrian Yates <adeyatz@gmail.com>
2023-10-05 20:38:01 +0100

    refactor: VSCode Format Document

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,36 +1,36 @@
 export function getSquares(numbers) {
 	if (numbers === undefined) throw new Error('nums is required');
-	
-	return numbers.map (number => number * number);
+
+	return numbers.map(number => number * number);
 }
 
 export function camelCaseWords(words) {
 	if (words === undefined) throw new Error('words is required');
-	
-	return words.reduce ((current, word) => current + word.charAt(0).toUpperCase() + word.slice(1));
+
+	return words.reduce((current, word) => current + word.charAt(0).toUpperCase() + word.slice(1));
 }
 
 export function getTotalSubjects(people) {
 	if (people === undefined) throw new Error('people is required');
-	
-	return people.reduce ((previousTotal, person) => previousTotal + person.subjects.length,0);
+
+	return people.reduce((previousTotal, person) => previousTotal + person.subjects.length, 0);
 }
 
 export function checkIngredients(menu, ingredientToFind) {
 	if (menu === undefined) throw new Error('menu is required');
 	if (!ingredientToFind) throw new Error('ingredient is required');
-	
+
 	return menu.filter(meal => meal.ingredients
-					.filter (mealIngredient => mealIngredient === ingredientToFind).length > 0).length > 0;
+		.filter(mealIngredient => mealIngredient === ingredientToFind).length > 0).length > 0;
 }
 
 export function duplicateNumbers(arr1, arr2) {
 	if (arr1 === undefined) throw new Error('arr1 is required');
 	if (arr2 === undefined) throw new Error('arr2 is required');
-	
-	const sortedArrayWithduplicates =  arr1.filter (num1 => arr2
-											.filter (num2 => num1 === num2 ).length > 0)
-											.sort ((a,b) => a-b);
 
-	return sortedArrayWithduplicates.filter ((item, index) => sortedArrayWithduplicates.indexOf(item) === index);
+	const sortedArrayWithduplicates = arr1.filter(num1 => arr2
+		.filter(num2 => num1 === num2).length > 0)
+		.sort((a, b) => a - b);
+
+	return sortedArrayWithduplicates.filter((item, index) => sortedArrayWithduplicates.indexOf(item) === index);
 }
