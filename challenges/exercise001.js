41b57a6258a9756db6629ec9311dc7075b2649e9
Adrian Yates <adeyatz@gmail.com>
2023-10-05 19:16:28 +0100

    refactor: applied VSCode Format Document to exercise001 and exercise002

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -17,10 +17,10 @@ export function capitalize(word) {
 export function generateInitials(firstName, lastName) {
 	if (firstName === undefined) throw new Error('firstName is required');
 	if (lastName === undefined) throw new Error('lastName is required');
-	
+
 	const firstInitial = (firstName.length == 0) ? "" : firstName.charAt(0).toUpperCase();
 	const lastInitial = (lastName.length == 0) ? "" : lastName.charAt(0).toUpperCase();
-	
+
 	return firstInitial + "." + lastInitial;
 }
 
@@ -29,38 +29,38 @@ export function addVAT(originalPrice, vatRate) {
 		throw new Error('originalPrice is requied');
 	if (vatRate === undefined) throw new Error('vatRate is required');
 
-	return originalPrice + (Math.round ((originalPrice * (vatRate/100)) * 100)/100);
+	return originalPrice + (Math.round((originalPrice * (vatRate / 100)) * 100) / 100);
 }
 
 export function getSalePrice(originalPrice, reduction) {
 	if (originalPrice === undefined)
 		throw new Error('originalPrice is required');
-	if (reduction === undefined) 
+	if (reduction === undefined)
 		throw new Error('reduction is required');
 	if (reduction > 100)
-		throw new Error ('reduction cannot be greater that 100%');
-	
-	return Math.round ((originalPrice * ((100 - reduction)/100)) * 100)/100;
+		throw new Error('reduction cannot be greater that 100%');
+
+	return Math.round((originalPrice * ((100 - reduction) / 100)) * 100) / 100;
 }
 
 export function getMiddleCharacter(str) {
 	if (str === undefined) throw new Error('str is required');
 	if (str.length === 0)
-		throw new Error ('str is empty');
+		throw new Error('str is empty');
 
 	const myString = str.trim();
-	const startPos = Math.floor((myString.length + 1)/2) - 1;
+	const startPos = Math.floor((myString.length + 1) / 2) - 1;
 	const replyStringLength = (myString.length % 2 === 0) ? 2 : 1;
-	
-	return  myString.substring (startPos, startPos + replyStringLength);
+
+	return myString.substring(startPos, startPos + replyStringLength);
 
 }
 
 export function reverseWord(word) {
 	if (word === undefined) throw new Error('word is required');
 
-	if (word.length === 0) throw new Error ('word length is zero');
-	
+	if (word.length === 0) throw new Error('word length is zero');
+
 	const splitString = word.split("");
 
 	const reverseArray = splitString.reverse();
@@ -70,29 +70,29 @@ export function reverseWord(word) {
 
 export function reverseAllWords(words) {
 	if (words === undefined) throw new Error('words is required');
-	if (words.length === 0) throw new Error ('words are not there');
+	if (words.length === 0) throw new Error('words are not there');
 
-	return words.map (word =>  reverseWord(word));
+	return words.map(word => reverseWord(word));
 }
 
 export function countLinuxUsers(users) {
 	if (users === undefined) throw new Error('users is required');
 
-	return users.filter (user => user.type ==="Linux").length;
+	return users.filter(user => user.type === "Linux").length;
 }
 
 export function getMeanScore(scores) {
 	if (scores === undefined) throw new Error('scores is required');
 
-	if (scores.length === 0) throw new Error ('scores is empty');
+	if (scores.length === 0) throw new Error('scores is empty');
 
-	const sum = scores.reduce ((total, score) => {return total + score}, 0);
-	return Math.round ((sum/scores.length) * 100)/100;
+	const sum = scores.reduce((total, score) => { return total + score }, 0);
+	return Math.round((sum / scores.length) * 100) / 100;
 }
 
 export function simpleFizzBuzz(inputValue) {
 	if (inputValue === undefined) throw new Error('n is required');
-	
+
 	if (inputValue % 3 === 0 && inputValue % 5 === 0)
 		return "fizzbuzz";
 	else if (inputValue % 3 === 0)
@@ -101,5 +101,5 @@ export function simpleFizzBuzz(inputValue) {
 		return 'buzz';
 	else
 		return inputValue;
-	
+
 }
--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,30 +1,30 @@
 export function getFillings(sandwich) {
 	if (sandwich === undefined) throw new Error('ingredients is required');
-	
+
 	return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
 	if (person === undefined) throw new Error('person is required');
-	
+
 	return person.city === 'Manchester';
 }
 
 export function getBusNumbers(people) {
 	if (people === undefined) throw new Error('people is required');
-	
-	return Math.ceil (people/40);
+
+	return Math.ceil(people / 40);
 }
 
 export function countSheep(animals) {
 	if (animals === undefined) throw new Error('arr is required');
-	
-	return animals.filter (animal => animal === 'sheep').length;
+
+	return animals.filter(animal => animal === 'sheep').length;
 
 }
 
 export function hasMPostCode(person) {
 	if (person === undefined) throw new Error('person is required');
-	
-	return /^[M][0-9]/.test (person.address.postCode);
+
+	return /^[M][0-9]/.test(person.address.postCode);
 }
