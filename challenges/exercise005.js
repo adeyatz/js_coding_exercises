5767902dc8731f75f8fd233f55fa3c31edaf499f
Adrian Yates <adeyatz@gmail.com>
2023-10-06 18:55:51 +0100

    feat: impl. findNeedle

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -77,7 +77,15 @@ export const arrShift = (arr) => {
 export const findNeedle = (haystack, searchTerm) => {
 	if (haystack === undefined) throw new Error('haystack is required');
 	if (searchTerm === undefined) throw new Error('searchTerm is required');
-	// Your code here!
+
+	for (let key in haystack) {
+		if (typeof haystack[key] === "string") {
+			if (haystack[key].toLowerCase().includes(searchTerm.toLowerCase())) {
+				return true;
+			}
+		}
+	}
+	return false;
 };
 
 export const getWordFrequencies = (str) => {
